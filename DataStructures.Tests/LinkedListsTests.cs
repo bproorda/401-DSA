213c7a8b0cb05be5e0c50b16c37fe96e6078dc31
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-08 18:49:39 -0500

    added insert method and related tests

--- DataStructures.Tests/LinkedListsTests.cs
+++ DataStructures.Tests/LinkedListsTests.cs
@@ -5,8 +5,8 @@ namespace DataStructures.Tests
 {
     /*
     1. Can successfully instantiate an empty linked list DONE
-    2. Can properly insert into the linked list
-    3. The head property will properly point to the first node in the linked list
+    2. Can properly insert into the linked list DONE
+    3. The head property will properly point to the first node in the linked list DONE
     4. Can properly insert multiple nodes into the linked list
     5. Will return true when finding a value within the linked list that exists
     6. Will return false when searching for a value in the linked list that does not exist
@@ -16,11 +16,41 @@ namespace DataStructures.Tests
     {
         [Fact]
         public void Can_Create_Empty_List()
-        {   //Arrange, Act
+        {   //Act
             DataStructures.LinkedLists testList = new LinkedLists();
 
             //Assert
             Assert.Equal("", testList.ToString());
         }
+
+        [Fact]
+        public void Can_Insert_Node()
+        {
+            //Arrange
+            DataStructures.LinkedLists testList = new LinkedLists();
+
+            //Act
+            testList.Insert(13);
+
+            //Assert
+            Assert.Equal("13", testList.ToString());
+        }
+
+        [Fact]
+        public void Head_Points_To_Next()
+        {
+            //Arrange
+            DataStructures.LinkedLists testList = new LinkedLists();
+            int expected = 13;
+            testList.Insert(expected);
+
+            //Act
+            testList.Insert(12);
+            int result = testList.Head.Next.Value;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
     }
 }
--- DataStructures/LinkedLists.cs
+++ DataStructures/LinkedLists.cs
@@ -3,12 +3,13 @@ using System;
 
 namespace DataStructures
 {
-    // TODO: Create Insert method to add node to LinkedList and adds new value to LinkedList.head
+    // TODO: Create Insert method to add node to LinkedList and adds new value to LinkedList.head DONE
     // TODO: Create Includes method thats to see if a given value is in the LinkedList and returns a boolean
     // TODO: Define method ToString which returns a string representing all the items in the list, see assignment for exmaple.
     public class LinkedLists
     {
         public Node Head = null;
+        public Node Current = null;
 
         public class Node
         {
@@ -23,7 +24,30 @@ namespace DataStructures
 
         public override string ToString()
         {
-            return "";
+            /*string output = "";
+            if (this.Head == null)
+            {
+                 output = $"{this.Head.Value}";
+            }
+
+            return output; */
+
+            if (Head == null)
+            {
+                return "";
+            } else
+            {
+                return Head.Value.ToString();
+            }
+
+        }
+
+        public void Insert(int newValue)
+        {
+            Node newNode = new Node(newValue);
+            if(Head != null)
+            { newNode.Next = Head; }
+            Head = newNode;
         }
     }
 }
