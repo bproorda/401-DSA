92c43dbe0730b712f5f59a825b5e72a783f4f5a5
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-08 17:58:12 -0500

    setup, added new class, constructor, and empty list test

---
+++ DataStructures/LinkedLists.cs
@@ -0,0 +1,24 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+
+namespace DataStructures
+{
+    // TODO: Create Insert method to add node to LinkedList and adds new value to LinkedList.head
+    // TODO: Create Includes method thats to see if a given value is in the LinkedList and returns a boolean
+    // TODO: Define method ToString which returns a string representing all the items in the list, see assignment for exmaple.
+    public class LinkedLists
+    {
+        public Node Head = null;
+
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Next { get; set; }
+            public Node(int value)
+            {
+                this.Value = value;
+                this.Next = null;
+        }
+        }
+    }
+}
