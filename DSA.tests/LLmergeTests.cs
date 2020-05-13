28cb09d1258669e6c9218945e9a7f6bbe4899f3c
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-13 18:26:40 -0500

    created test for merge method

---
+++ DSA.tests/LLmergeTests.cs
@@ -0,0 +1,35 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DSA.tests
+{
+    
+    public class LLmergeTests
+    {
+
+        [Fact]
+        public void Can_Merge_Test()
+        {   
+            //arrange
+            DataStructures.LinkedLists listOne = new LinkedLists();
+            listOne.Insert(5);
+            listOne.Insert(10);
+            listOne.Insert(15);
+            DataStructures.LinkedLists listTwo = new LinkedLists();
+            listOne.Insert(6);
+            listOne.Insert(12);
+            listOne.Insert(18);
+            string expected = "{5} => {6} => {10} => {12} => {15} => {18} => NULL";
+
+            //act
+            DataStructures.LinkedLists actualList = LLchallenges.mergeLists(listOne, listTwo);
+            string actual = actualList.ToString();
+
+            //Assert
+            Assert(expected, actual);
+        }
+    }
+}
---
+++ DSA/LLchallenges.cs
@@ -0,0 +1,15 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class LLchallenges
+    {
+        public LinkedLists mergeLists(LinkedLists lOne, LinkedLists lTwo)
+        {
+            return lOne;
+        }
+    }
+}
