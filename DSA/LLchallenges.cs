751ef9f50a1dc1be6cd5ae243fb0aadd0b25abef
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-13 19:08:00 -0500

    worked out method, test is passing!

--- DSA/LLchallenges.cs
+++ DSA/LLchallenges.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static DataStructures.LinkedLists;
 
 namespace DSA
 {
@@ -9,7 +10,32 @@ namespace DSA
     {
         public static LinkedLists mergeLists(LinkedLists lOne, LinkedLists lTwo)
         {
-            return lOne;
+            LinkedLists mergedList = new LinkedLists();
+            Node mCurrent = null;
+            Node l1Current = lOne.Head;
+            Node l2Current = lTwo.Head;
+            mCurrent = l1Current;
+            mergedList.Head = mCurrent;
+            l1Current = l1Current.Next;
+
+            while (l1Current != null || l2Current != null)
+            {
+                if (l2Current != null)
+                {
+                    mCurrent.Next = l2Current;
+                    l2Current = l2Current.Next;
+                    mCurrent = mCurrent.Next;
+                }
+
+                if (l1Current != null)
+                {
+                    mCurrent.Next = l1Current;
+                    l1Current = l1Current.Next;
+                    mCurrent = mCurrent.Next;
+                }
+
+            }
+            return mergedList;
         }
     }
 }
