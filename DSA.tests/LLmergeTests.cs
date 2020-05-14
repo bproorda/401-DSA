abd66c162922b63e8603d52bb175e87596e9ad82
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-13 19:23:04 -0500

    added test for lists of different lengths, works if the longer list is first , but not if longer list is the second parameter. might save it for code review, need to work on lab

--- DSA.tests/LLmergeTests.cs
+++ DSA.tests/LLmergeTests.cs
@@ -31,5 +31,33 @@ namespace DSA.tests
             //Assert
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Can_Merge_Lists_Of_Different_Lengths_Test()
+        {
+            //arrange
+            DataStructures.LinkedLists listOne = new LinkedLists();
+            listOne.Insert(15);
+            listOne.Insert(10);
+            listOne.Insert(5);
+            DataStructures.LinkedLists listTwo = new LinkedLists();
+            listTwo.Insert(12);
+            listTwo.Insert(6);
+            string expected = "{5} => {6} => {10} => {12} => {15} => NULL";
+
+            //act
+            DataStructures.LinkedLists actualList = LLchallenges.mergeLists(listOne, listTwo);
+            string actual = actualList.ToString();
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+            //act
+            //DataStructures.LinkedLists actualList2 = LLchallenges.mergeLists(listTwo, listOne);
+            //string actual2 = actualList.ToString();
+
+            //Assert
+            //Assert.Equal(expected, actual2);
+        }
     }
 }
