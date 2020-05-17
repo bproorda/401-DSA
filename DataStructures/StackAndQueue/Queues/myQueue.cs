bbdd2cda0717c5347202b6b78dbef3f2cb1119f2
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-16 20:21:42 -0500

    added Readme for Stack and started on class for queue

---
+++ DataStructures/StackAndQueue/Queues/myQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class myQueue
+    {
+        public Node Top { get; set; } = null;
+        public Node Rear { get; set; } = null;
+        public class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Next = null;
+            }
+        }
+    }
+}
