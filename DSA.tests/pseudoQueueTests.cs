624b13ad8ac25f11681ad773762aba8b9b675efe
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-23 17:05:44 -0500

    created basic pseudoQueue class with empty methods and test to see if creation of empty PQ was possible. Green

---
+++ DSA.tests/pseudoQueueTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DSA.tests
+{
+   public class pseudoQueueTests
+    {
+        [Fact]
+
+        public void Can_Create_Empty_PseudoQueue()
+        {
+            //act
+            pseudoQueue<int> testPQ = new DSA.pseudoQueue<int>();
+
+            //assert
+            Assert.NotNull(testPQ);
+        }
+
+    }
+}
---
+++ DSA/pseudoQueue.cs
@@ -0,0 +1,23 @@
+﻿using DataStructures.StackAndQueue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class pseudoQueue<T>
+    {
+        public MyStack<T> addStack = new MyStack<T>();
+        public MyStack<T> subtractStack = new MyStack<T>();
+
+        public void enqueue(T value)
+        {
+
+        }
+
+        public T dequeue()
+        {
+            return default;
+        }
+    }
+}
