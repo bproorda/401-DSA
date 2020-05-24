63df8e4167a088441128f1aef766e2b15ff2dfc0
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-23 20:48:19 -0500

    basic scaffoling of classes

---
+++ DSA/AnimalShelter/dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.AnimalShelter
+{
+    public class dog : Animal
+    {
+        public override string Name { get; set; }
+        public override bool IsGoodNotEvil { get => true; }
+
+        public dog(string name)
+        {
+            Name = name;
+        }
+    }
+}
