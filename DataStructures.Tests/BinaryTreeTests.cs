879990ab2e2e83cef727c9ec41042d010a2f58cd
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-25 20:27:41 -0500

    basic scaffolding

---
+++ DataStructures.Tests/BinaryTreeTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class BinaryTreeTests
+    {
+        [Fact]
+
+        public void Can_Create_Empty_Tree()
+        {
+
+        }
+    }
+}
