36172980ffbca02b8858e44234b8d96c728ee70e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-25 20:57:57 -0500

    created add method

--- DataStructures/BinaryTree.cs
+++ DataStructures/BinaryTree.cs
@@ -1,25 +1,69 @@
-﻿using System;
+﻿using DataStructures;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataStructures
 {
-    public class BinaryTree<T>
+    public class BinaryTree
     {
-        Node Root { get; set; }
+        public Node Root { get; set; }
 
         public class Node 
         {
-            T Value { get; set; }
-            Node Left { get; set; }
-            Node Right { get; set; }
+           public int Value { get; set; }
+            public Node Left { get; set; }
+          public  Node Right { get; set; }
 
         }
 
     }
 
-    public class BinarySearchTree : BinaryTree<int>
+    public class BinarySearchTree : BinaryTree
     {
+   
 
+    public void add(int value)
+        {
+            Node newNode = new Node();
+            newNode.Value = value;
+        if (Root == null)
+        {
+            Root = newNode;
+        } else
+        {
+            Node current = Root;
+                bool toggle = true;
+                while (toggle)
+                {
+                    if (value > current.Value)
+                    {
+                        if (current.Right == null)
+                        {
+                            current.Right = newNode;
+                            break;
+                        } else
+                        {
+                            current = current.Right;
+                        }
+                    }
+                    else if (value <= current.Value)
+                    {
+                        if (current.Left == null)
+                        {
+                            current.Left = newNode;
+                            break;
+                        } else
+                        {
+                            current = current.Left;
+                        }
+                    }
+
+
+                }
+        }
+        }
+
+    }
     }
 }
