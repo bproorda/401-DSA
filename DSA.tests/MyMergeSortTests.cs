57f46850c3e4275bca8e2c5784462f738a6d3911
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-09 13:49:45 -0500

    put all insertion sort files into a folder and scaffolded out some of the files for Merge Sort

---
+++ DSA.tests/MyMergeSortTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DSA.tests
+{
+    public class MyMergeSortTests
+    {
+        [Fact]
+        public void Merge_Sort_Test_One()
+        { 
+        }
+    }
+}
---
+++ DSA/Sorting-Week6/MergeSort/MyMergeSort.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Sorting_Week6.MergeSort
+{
+    class MyMergeSort
+    {
+    }
+}
