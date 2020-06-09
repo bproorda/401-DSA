97ff67fea54bf0ff57cec746eb83b21851aea899
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-08 21:18:13 -0500

    implemented InsertionSort method, it passes!

--- DSA/Sorting-Week6/InsertSort.cs
+++ DSA/Sorting-Week6/InsertSort.cs
@@ -14,7 +14,21 @@ namespace DSA.Sorting_Week6
                 throw new EmptyArrayException();
             }
 
-            return default;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int j = i - 1;
+                int temp = input[i];
+
+                while (j>=0 && temp < input[j])
+                {
+                    input[j + 1] = input[j];
+                    j = j - 1;
+                }
+                input[j + 1] = temp;
+
+            }
+
+            return input;
         }
     }
 
