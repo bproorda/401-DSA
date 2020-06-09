b501c7114c9d5fa0c8dc8d92faba2bf45464774d
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-09 14:13:10 -0500

    wrote out mergesort and merge, and yes i know am doing ti backwards this time

--- DSA/Sorting-Week6/MergeSort/MyMergeSort.cs
+++ DSA/Sorting-Week6/MergeSort/MyMergeSort.cs
@@ -6,5 +6,68 @@ namespace DSA.Sorting_Week6.MergeSort
 {
     class MyMergeSort
     {
+        public static int[] MergeSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            if (n > 1)
+            {
+                int mid = n / 2;
+                int[] left = new int[mid];
+                int[] right = new int[n - mid];
+                for (int i = 0; i < left.Length; i++)
+                {
+                    left[i] = arr[i];
+                    right[i] = arr[i + mid + 1];
+                }
+                MergeSort(left);
+                MergeSort(right);
+                return  Merge(left, right, arr);
+            }
+            return default;
+            
+        }
+
+        public static int[] Merge(int[] left, int[] right, int[] arr)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] < right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                } else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+
+                if (i == left.Length)
+                {
+                    while (j < right.Length)
+                    {
+                        arr[k] = right[j];
+                        j++;
+                        k++;
+                    }
+                }
+                if (j == right.Length)
+                {
+                    while (i < left.Length)
+                    {
+                        arr[k] = left[i];
+                        i++;
+                        k++;
+                    }
+                }
+
+            }
+            return arr;
+        }
     }
 }
