84fd853a0140452c86a8254230f6d9d0d1c06f47
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-10 17:50:36 -0500

    scaffolded files, empty method, and first test. RED

---
+++ DSA.tests/MyQuickSortTests.cs
@@ -0,0 +1,25 @@
+﻿using DSA.Sorting_Week6.QuickSort;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DSA.tests
+{
+    public class MyQuickSortTests
+    {
+        [Fact]
+        public void Does_Quick_Sort_Work()
+        {
+            //arrange 
+            int[] test = new int[] { 8, 5, 43, 78, 29, 36, 13 };
+            int[] expected = new int[] { 5, 8, 13, 36, 29, 43, 78 };
+
+            //act
+            int[] actual = MyQuickSort.QuickSort(test);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DSA/Sorting-Week6/QuickSort/MyQuickSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Sorting_Week6.QuickSort
+{
+    public class MyQuickSort
+    {
+        public static int[] QuickSort(int[] arr)
+        {
+            return default;
+        }
+    }
+}
