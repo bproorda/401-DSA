946dffa36790ca5b45d1c9415b189b79f2458874
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-13 19:18:00 -0500

    started working on IEqualityComparer implementation and test

---
+++ DataStructures.Tests/MyHashTableTests.cs
@@ -0,0 +1,21 @@
+﻿using DataStructures.myHashTable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class MyHashTableTests
+    {
+        [Fact(Skip ="Not Ready Yet")]
+        public void Get_Hash_Code_Works()
+        {
+            //act
+            //int testHash = MyEqualityComparer.GetHashCode("Brennan");
+
+            //assert
+            //Assert.Equal(11, testHash);
+        }
+    }
+}
---
+++ DataStructures/myHashTable/MyEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.myHashTable
+{
+    public class MyEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode(string key)
+        {
+            int keyLength = key.Length;
+
+            int[] asciiArr = new int[keyLength]; 
+
+            for (int i = 0; i < keyLength; i++)
+            {
+                asciiArr[i] = (int)key[i];
+            }
+            return 1;
+        }
+    }
+}
