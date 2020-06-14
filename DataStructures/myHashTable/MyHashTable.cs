a57e556517fe9b5ab78610d88f09d8c6780867d4
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-13 19:34:41 -0500

    added node class and myHashTable constructor

--- DataStructures/myHashTable/MyHashTable.cs
+++ DataStructures/myHashTable/MyHashTable.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace DataStructures.myHashTable
 {
-    class MyHashTable
+    public class MyHashTable<T>
     {
+        public MyEqualityComparer myEqualityComparer;
+
+        public MyHashTable(MyEqualityComparer myEqualityComparer)
+        {
+            this.myEqualityComparer = myEqualityComparer;
+        }
+
+
+        public class Node
+        {
+            public string Key { get; set; }
+
+            public T Value { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(string key, T value)
+            {
+                this.Key = key;
+                this.Value = value;
+            }
+        }
     }
 }
