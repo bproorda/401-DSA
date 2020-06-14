6c6a8d6681cf139533ce3b20e9d4e7a15bc27e09
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-13 19:26:14 -0500

    wrote out gethashcode function, will see if it works later

--- DataStructures/myHashTable/MyEqualityComparer.cs
+++ DataStructures/myHashTable/MyEqualityComparer.cs
@@ -15,13 +15,17 @@ namespace DataStructures.myHashTable
         {
             int keyLength = key.Length;
 
-            int[] asciiArr = new int[keyLength]; 
+            //int[] asciiArr = new int[keyLength];
+            int asciiChar;
+            int result = 0;
 
             for (int i = 0; i < keyLength; i++)
             {
-                asciiArr[i] = (int)key[i];
+                //asciiArr[i] = (int)key[i];
+                asciiChar = (int)key[i];
+                result += (int)Math.Pow(asciiChar, (keyLength - i));
             }
-            return 1;
+            return result * 613;
         }
     }
 }
