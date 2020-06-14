7be02f42cf4c74caf2f301f715d0ff70eaf0bf11
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-13 22:24:06 -0500

    a little cleanup

--- DataStructures/myHashTable/MyHashTable.cs
+++ DataStructures/myHashTable/MyHashTable.cs
@@ -34,27 +34,7 @@ namespace DataStructures.myHashTable
             }
         }
 
-       /* public  int GetHashCode(string key, int bucketCount)
-        {
-            int keyLength = key.Length;
 
-            int asciiChar;
-            int result = 0;
-
-            for (int i = 0; i < keyLength; i++)
-            {
-                asciiChar = (int)key[i];
-                result += (int)Math.Pow(asciiChar, (keyLength - i));
-            }
-            if (result < 0)
-            {
-                result  = result * -1;
-            }
-            int divisor = 59233;
-            result = result / divisor;
-            result = result % bucketCount;
-            return result;
-        }*/
 
         public void Add(string key, T value)
         {
