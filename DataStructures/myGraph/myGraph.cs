4cfc2c926029a68be77c424ca8649a6cf94f3b09
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-21 17:37:17 -0500

    fixed quick error

--- DataStructures/myGraph/myGraph.cs
+++ DataStructures/myGraph/myGraph.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace DataStructures.myGraph
 {
-   public class myGraph
+   public class myGraph<T>
     {
+
+
+        public List<Vertex<T>> Vertices { get; set; }
+    }
+    public class Vertex<T>
+    {
+        public T Value { get; set; }
+
+        public List<Vertex<T>> Neighbors { get; set; }
+
+        public Vertex(T value)
+        {
+            this.Value = value;
+            this.Neighbors = new List<Vertex<T>>();
+        }
+
     }
 }
