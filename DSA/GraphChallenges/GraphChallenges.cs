41545960780bc7606c3ca3b8847496528a6134d9
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-23 20:22:28 -0500

    filled out method, not sure if it is correct. tired. Test time!

--- DSA/GraphChallenges/GraphChallenges.cs
+++ DSA/GraphChallenges/GraphChallenges.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.myGraph;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,43 @@ namespace DSA.GraphChallenges
 {
     public class GraphChallenges
     {
+        public TripResult PlaneTrip(myGraph<string> graph, string[] stops )
+        {
+            int length = stops.Length;
+            var current = graph.FindVertex(stops[0]);
+            int? sum = 0;
+
+
+            while (current != null)
+            {
+                int i = 1;
+                for (int j = 0; j < current.Neighbors.Count; j++)
+                {
+                    if (current.Neighbors[j].Neighbor.Value == stops[i])
+                    {
+                        sum += current.Neighbors[j].Weight;
+                        current = current.Neighbors[j].Neighbor;
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            
+            return default;
+        }
+        
+    }
+    public class TripResult
+    {
+        public bool pathFound { get; set; }
+
+        public int? cost { get; set; }
+
+        public TripResult(bool pathfound, int? cost)
+        {
+            this.pathFound = pathfound;
+            this.cost = cost;
+        }
     }
 }
